b1d3a28ba2aacd21b04183fba9146281fea2a038
sevocrear <sevocrear@gmail.com>
2020-05-30 18:10:38 +0300

    updated position control script

--- cheetah_control/scripts/control_pos.py
+++ cheetah_control/scripts/control_pos.py
@@ -7,52 +7,67 @@ import random
 import time
 import numpy as np
 
+class cheetah_control():
+	def __init__(self, type_of_control = 'position', time_pause_before_control = 5, rate_value = 50):
+		rospy.init_node('rotate', anonymous=True)
 
-def move_joint(pub, speed, upper_limit, lower_limit, rate_value):
-    i = rospy.get_time()
-    diff = (upper_limit - lower_limit)/2
-    offset = upper_limit - diff
-    position = np.sin(i/rate_value*speed)*diff + offset
-    # rospy.loginfo(position)
-    pub.publish(position)
+		self.type_of_control = type_of_control
+		self.time_pause_before_control = time_pause_before_control
 
-def move_joint_to_pos(pub, pos):
-    pub.publish(pos)	
+		time.sleep(self.time_pause_before_control)
 
-def joint_name(joint):
-    joint_name = '/mini_chee/'+joint+'/command'
-    return joint_name
+		self.joints = {1: 'FL_hip_position_controller', 2: 'FL_thigh_position_controller', 3: 'FL_calf_position_controller',
+
+		4: 'FR_hip_position_controller', 5: 'FR_thigh_position_controller', 6: 'FR_calf_position_controller',
+
+		7: 'RL_hip_position_controller', 8: 'RL_thigh_position_controller', 9: 'RL_calf_position_controller',
+
+		10: 'RR_hip_position_controller', 11: 'RR_thigh_position_controller', 12: 'RR_calf_position_controller'}
+		self.pub = {}
+		self.joints_number = len(self.joints)
+
+		for i in range(1,self.joints_number+1):
+			self.pub[i]=rospy.Publisher(self.joint_name(self.joints[i]), Float64, queue_size=10)
+
+		self.rate_value = rate_value
+
+
+
+
+	def move_joint_to_pos(self,i, pos):
+		"""
+
+		i - number of joint: 
+		1: 'FL_hip_position_controller', 
+		2: 'FL_thigh_position_controller', 
+		3: 'FL_calf_position_controller',
+		4: 'FR_hip_position_controller', 
+		5: 'FR_thigh_position_controller', 
+		6: 'FR_calf_position_controller',
+		7: 'RL_hip_position_controller', 
+		8: 'RL_thigh_position_controller', 
+		9: 'RL_calf_position_controller',
+		10: 'RR_hip_position_controller', 
+		11: 'RR_thigh_position_controller', 
+		12: 'RR_calf_position_controller';
+
+
+		pos - angle in rad
+		"""	
+		self.pub[i].publish(pos)	
+
+	def joint_name(self, joint):
+		joint_name = '/mini_chee/'+joint+'/command'
+		return joint_name
 
 
 if __name__ == '__main__':
-	speed = 100
-	upper_limit = np.pi
-	lower_limit = -np.pi
-	time.sleep(5)
-	rospy.init_node('rotate', anonymous=True)
+	cheetah_control_pos = cheetah_control()
 
-	joints = {1: 'FL_hip_position_controller', 2: 'FL_thigh_position_controller', 3: 'FL_calf_position_controller',
-
-	4: 'FR_hip_position_controller', 5: 'FR_thigh_position_controller', 6: 'FR_calf_position_controller',
-
-	7: 'RL_hip_position_controller', 8: 'RL_thigh_position_controller', 9: 'RL_calf_position_controller',
-
-	10: 'RR_hip_position_controller', 11: 'RR_thigh_position_controller', 12: 'RR_calf_position_controller'}
-	pub = {}
-	joints_number = len(joints)
-	for i in range(1,joints_number+1):
-		pub[i]=rospy.Publisher(joint_name(joints[i]), Float64, queue_size=10)
-
-
-	rate_value = 50  # 50hz
-	rate = rospy.Rate(rate_value)
-
-	while not 0:
+	while not rospy.is_shutdown():
 		try:
-			# move_joint(pub[3], speed, upper_limit, lower_limit,rate_value)
-			# move_joint(pub[6], speed, upper_limit, lower_limit, rate_value)
-			move_joint_to_pos(pub[3],np.pi/4)
-			move_joint_to_pos(pub[6], np.pi/4)
+			cheetah_control_pos.move_joint_to_pos(3,np.pi/4)
+			cheetah_control_pos.move_joint_to_pos(6, np.pi/4)
 		except rospy.ROSInterruptException:
 			pass
-		rate.sleep()
+	cheetah_control_pos.rate_value.sleep()
